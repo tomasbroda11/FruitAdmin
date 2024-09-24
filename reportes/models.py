eeefe0e6026eae9a643ba20596d9d119c8b5235d
Tomas Broda <tomasbroda13@gmail.com>
2024-09-24 17:13:26 -0300

    Grafivo barras listo

--- reportes/models.py
+++ reportes/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.shortcuts import render
 
-def dashboard_view(request):
-    # Lógica para el dashboard
-    return render(request, 'dashboard.html')
 
 
 # Create your models here.
