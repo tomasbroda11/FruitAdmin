12ec1815894616009f1d21e341985b3e75c3e68d
Tomas Broda <tomasbroda13@gmail.com>
2024-10-21 19:43:19 -0300

    Home page actualizada + migraciones

--- clientes/forms.py
+++ clientes/forms.py
@@ -11,11 +11,13 @@ class ClienteForm(forms.ModelForm):
 class ClienteUpdateForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = ['nombre', 'cuit','email', 'telefono', 'direccion']  # Agrega todos los campos que deseas actualizar.
+        fields = ['nombre', 'cuit','email', 'telefono', 'direccion','ciudad','provincia']  
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
             'cuit': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'telefono': forms.TextInput(attrs={'class': 'form-control'}),
             'direccion': forms.TextInput(attrs={'class': 'form-control'}),
+            'ciudad': forms.TextInput(attrs={'class': 'form-control'}),
+            'provincia': forms.Select(attrs={'class': 'form-control'}),
         }
